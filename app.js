6e1680e7f0a9a389cb3d2cb8c206bfb50586e2d8
pyoja <wjdgns13nr@naver.com>
2023-05-31 16:52:13 +0900

    노드 입문과제 pt1

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const postRouter = require("./routes/posts");
+
 const app = express();
+
+mongoose.connect("mongodb://localhost/node-basic");
+const db = mongoose.connection;
+
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("db connected!"));
+
+app.use(express.json());
+app.use("/posts", postRouter);
 
 app.listen(3000, () => {
   console.log("server start!!!");
